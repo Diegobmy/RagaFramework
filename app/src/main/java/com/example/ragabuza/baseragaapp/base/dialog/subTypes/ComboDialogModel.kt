0f38321f331196fd4af60b1163c7ae8f23d2ffa9
diegomoyses <diego.moyses@iteris.com.br>
2018-09-25 18:22:11 -0300

    Começo combo list

--- app/src/main/java/com/example/ragabuza/baseragaapp/base/dialog/subTypes/ComboDialogModel.kt
+++ app/src/main/java/com/example/ragabuza/baseragaapp/base/dialog/subTypes/ComboDialogModel.kt
@@ -1,21 +1,54 @@
 package com.example.ragabuza.baseragaapp.base.dialog.subTypes
 
+import android.support.v7.widget.LinearLayoutManager
+import android.widget.CheckBox
+import android.widget.TextView
+import com.example.ragabuza.baseragaapp.R
+import com.example.ragabuza.baseragaapp.base.Message
+import com.example.ragabuza.baseragaapp.base.SimpleListAdapter
+import com.example.ragabuza.baseragaapp.base.afterTextChanged
 import com.example.ragabuza.baseragaapp.base.dialog.DialogModel
 import com.example.ragabuza.baseragaapp.base.dialog.RagaDialog
+import com.example.ragabuza.baseragaapp.base.setVisible
+import kotlinx.android.synthetic.main.dialog_combo.*
 
 class ComboDialogModel<T>(val builder: Builder<T>) : DialogModel(builder) {
+
+    val selected: MutableList<T> = mutableListOf()
+
     override fun RagaDialog.setInfo() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        setContentView(R.layout.dialog_combo)
+
+        closeButtonView = dialog_close
+        titleView = dialog_title
+        backgroungView = dialog_bg
+
+
+        dialog_recycler.layoutManager = LinearLayoutManager(context)
+        val simpleAdapter = SimpleListAdapter(builder.list, R.layout.combo_item)
+                .forId<CheckBox>(R.id.checkbox) { _, view, item ->
+                    view.text = builder.getItemLabel(item)
+                    view.setOnCheckedChangeListener { buttonView, isChecked ->
+                        if (isChecked)
+                            selected.add(item)
+                            else
+                            selected.remove(item)
+                    }
+                }
+
+        dialog_recycler.adapter = simpleAdapter
+        dialog_positive.setOnClickListener {
+            builder.positive?.second?.invoke(selected)
+        }
+
     }
 
 
     class Builder<T> : DialogModel.Builder(){
-        var hasFilter = false
-
         val list = mutableListOf<T>()
-        var getItemLabel: ((T) -> String)? = null
+        var getItemLabel: (item: T) -> String = { it.toString() }
 
-        var onConfirm: ((List<T>, List<Int>) -> Unit)? = null
+        var positive: Pair<Message, ((List<T>) -> Unit)>? = null
     }
 
 }
--- app/src/main/java/com/example/ragabuza/baseragaapp/main/MainActivity.kt
+++ app/src/main/java/com/example/ragabuza/baseragaapp/main/MainActivity.kt
@@ -2,11 +2,13 @@ package com.example.ragabuza.baseragaapp.main
 
 import android.os.Bundle
 import android.os.Handler
+import android.widget.Toast
 import com.example.ragabuza.baseragaapp.R
 import com.example.ragabuza.baseragaapp.base.BaseActivity
 import com.example.ragabuza.baseragaapp.base.Message
 import com.example.ragabuza.baseragaapp.base.dialog.DialogModel
 import com.example.ragabuza.baseragaapp.base.dialog.subTypes.ProgressDialogModel
+import com.example.ragabuza.baseragaapp.base.pairWith
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
@@ -17,30 +19,23 @@ class MainActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val progress = DialogModel.progress {
-            message = Message("nice")
-            title = Message("nice nice nice nice")
-            estimatedTime = 40000
-            onComplete = {
-                it.dismiss()
-            }
+        val progress = DialogModel.combo<String> {
+            list.add("1")
+            list.add("2")
+            list.add("3")
+            list.add("4")
+            list.add("5")
+
+            positive = Message("conf") pairWith { list -> Toast.makeText(applicationContext, list.toString(), Toast.LENGTH_SHORT).show() }
         }
 
-        val handler = Handler()
-        val runnable = object : Runnable {
-            override fun run() {
-                progress.percentage += 10
-                if (progress.percentage < 100)
-                    handler.postDelayed(this, 2000)
-            }
-        }
+
 
 //Start
 
 
         save.setOnClickListener {
             showDialog(progress)
-            handler.postDelayed(runnable, 2000)
         }
 
 
